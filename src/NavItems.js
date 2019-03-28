1127306a441bac94f92558146fe53c7089d6a4c7
kzielinski96 <kornel.zielinski96@gmail.com>
2019-03-28 22:49:59 +0100

    Fixed comments

--- src/NavItems.js
+++ src/NavItems.js
@@ -4,12 +4,7 @@ import './Navbar.scss';
 
 const NavItems = props => {
   return (
-    <Menu.Menu
-      className="items"
-      id="items"
-      position={props.position}
-      onClick={props.toggleMenu}
-    >
+    <Menu.Menu position={props.position} onClick={props.toggleMenu}>
       <Menu.Item as="a">
         <Icon size="large" name="user" />
       </Menu.Item>
@@ -24,94 +19,3 @@ const NavItems = props => {
 };
 
 export default NavItems;
-
-// const NavBarMobile = ({ onPusherClick, onToggle, visible }) => (
-//   <Sidebar.Pushable as={Segment}>
-//     <Sidebar
-//       as={Menu}
-//       animation="overlay"
-//       direction="right"
-//       Icon="labeled"
-//       inverted
-//       vertical
-//       inline
-//       visible={visible}
-//       width="thin"
-//     >
-//       <Menu.Item as="a">
-//         <Icon disabled name="user" /> User
-//       </Menu.Item>
-//       <Menu.Item as="a">
-//         <Icon disabled name="add" /> Add
-//       </Menu.Item>
-//       <Menu.Item as="a">
-//         <Icon disabled name="setting" /> Settings
-//       </Menu.Item>
-//     </Sidebar>
-
-//     <Sidebar.Pusher
-//       dimmed={visible}
-//       onClick={onPusherClick}
-//       style={{ minHeight: '100vh' }}
-//     >
-//       <Menu fixed="top" inverted segment>
-//         <Menu.Item as="a">
-//           <img src="#" alt="Logo" />
-//         </Menu.Item>
-//         <Menu.Menu position="right">
-//           <Menu.Item onClick={onToggle}>
-//             <Icon name="sidebar" />
-//           </Menu.Item>
-//         </Menu.Menu>
-//       </Menu>
-//     </Sidebar.Pusher>
-//   </Sidebar.Pushable>
-// );
-
-// const NavBarDesktop = () => (
-//   <Menu fixed="top" inverted segment>
-//     <Menu.Item as="a">
-//       <img src="#" alt="Logo" />
-//     </Menu.Item>
-//     <Menu.Menu position="right">
-//       <Menu.Item as="a">
-//         <Icon disabled name="user" />
-//       </Menu.Item>
-//       <Menu.Item as="a">
-//         <Icon disabled name="add" />
-//       </Menu.Item>
-//       <Menu.Item as="a">
-//         <Icon disabled name="setting" />
-//       </Menu.Item>
-//     </Menu.Menu>
-//   </Menu>
-// );
-
-// export default class Navbar extends Component {
-//   state = { visible: false };
-
-//   handlePusher = () => {
-//     const { visible } = this.state;
-
-//     if (visible) this.setState({ visible: false });
-//   };
-
-//   handleToggle = () => this.setState({ visible: !this.state.visible });
-
-//   render() {
-//     return (
-//       <div>
-//         <Responsive {...Responsive.onlyMobile}>
-//           <NavBarMobile
-//             onPusherClick={this.handlePusher}
-//             onToggle={this.handleToggle}
-//             visible={this.state.visible}
-//           />
-//         </Responsive>
-//         <Responsive minWidth={Responsive.onlyTablet.minWidth}>
-//           <NavBarDesktop />
-//         </Responsive>
-//       </div>
-//     );
-//   }
-// }
