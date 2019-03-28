8cce58ea4088f3c685e944cf1631d72c7a44b5fc
kzielinski96 <kornel.zielinski96@gmail.com>
2019-03-28 22:23:05 +0100

    Added responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import NavBar from './NavBar';
 
 const App = () => {
   return (
     <div className="App">
+      <div>
+        <NavBar />
+      </div>
       <header className="App-header">
         <h1>Welcome to Wasmo!</h1>
       </header>
