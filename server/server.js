449b291d3e1130348bb3ee61e5bbb9d3e8d5458a
krokowski0x <krokowski.dx@gmail.com>
2019-04-02 16:57:58 +0200

    Added simple Express server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const handlers = require('./handlers/user');
+
+const app = express();
+const API_PORT = 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Wasmo!' });
+});
+app.get('/users', handlers.getUsers);
+app.get('/users/:id', handlers.getUserById);
+app.post('/users', handlers.createUser);
+app.put('/users/:id', handlers.updateUser);
+app.delete('/users/:id', handlers.deleteUser);
+
+app.listen(API_PORT, () => {
+  console.log(`App running on port ${API_PORT}.`);
+});
