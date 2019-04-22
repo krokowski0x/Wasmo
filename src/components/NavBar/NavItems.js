5dab3871ea1630557e546103aac9903654093dde
krokowski0x <krokowski.dx@gmail.com>
2019-04-22 15:28:10 +0200

    Added dummy routes to NavBar

--- src/components/NavBar/NavItems.js
+++ src/components/NavBar/NavItems.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { Menu, Icon, Button } from 'semantic-ui-react';
 import './Navbar.scss';
 
@@ -9,24 +10,30 @@ const NavItems = props => {
   return (
     <Menu.Menu position={position} onClick={toggleMenu}>
       <Menu.Item as="a">
-        <Icon size="large" name="user" />
+        <Link to='/me'>
+          <Icon size="large" name="user" />
+        </Link>
       </Menu.Item>
       <Menu.Item as="a">
-        <Icon size="large" name="add" />
+        <Link to='/upload'>
+          <Icon size="large" name="add" />
+        </Link>
       </Menu.Item>
       <Menu.Item as="a">
-        <Icon size="large" name="setting" />
+        <Link to='/settings'>
+          <Icon size="large" name="setting" />
+        </Link>
       </Menu.Item>
-        <Menu.Menu position="right">
-          <Menu.Item>
-            <Button animated onClick={() => handleLogout()}>
-              <Button.Content visible>Log out</Button.Content>
-              <Button.Content hidden>
-                <Icon name="log out" />
-              </Button.Content>
-            </Button>
-          </Menu.Item>
-        </Menu.Menu>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Button animated onClick={() => handleLogout()}>
+            <Button.Content visible>Log out</Button.Content>
+            <Button.Content hidden>
+              <Icon name="log out" />
+            </Button.Content>
+          </Button>
+        </Menu.Item>
+      </Menu.Menu>
     </Menu.Menu>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
+
 import history from './history';
 import Auth from "./Auth";
+
 import Login from './components/LoginPage/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import Game from './components/Game/Game';
 import NavBar from './components/NavBar/NavBar';
+import Settings from './components/Settings/Settings';
+import Upload from './components/Upload/Upload';
+import UserDashboard from './components/User/UserDashboard';
 
 class WasmoRouter extends Component {
   constructor(props) {
@@ -37,6 +42,9 @@ class WasmoRouter extends Component {
           <NavBar handleLogout={this.logout} />
           <Route exact path='/' component={Dashboard} />
           <Route path='/login' render={(props) => <Login auth={auth} {...props} />} />
+          <Route path="/me" component={UserDashboard} />
+          <Route path="/upload" component={Upload} />
+          <Route path="/settings" component={Settings} />
           <Route path="/game/:title" component={Game} />
         </main>
       </Router>
