c5ee0176a1f77aacd698b0819bd0660c236c7bd5
krokowski0x <krokowski.dx@gmail.com>
2019-05-15 20:40:57 +0200

    Added basic User Dashboard view

--- src/components/User/UserDashboard.js
+++ src/components/User/UserDashboard.js
@@ -1,32 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-import { Container, Loader, Segment, Card, Image } from "semantic-ui-react";
+import { Card, Icon, Image, Grid, Segment, Loader } from 'semantic-ui-react'
 
 export default class UserDashboard extends Component {
-  constructor() {
-    super();
+constructor() {
+  super();
 
-    this.state = {
-      userData: {},
-      arraysReady: false,
-    };
-  }
+  this.state = {
+    gameList: [],
+    arraysReady: false,
+  };
+}
 
-	componentDidMount() {
-    fetch('/users/1')
-      .then(res => res.json())
-      .then(userData => this.setState({ userData, arraysReady: true }))
-      .catch(e => console.log(e));
-  }
-  
-  render() {
-    const { userData, arraysReady } = this.state;
+componentDidMount() {
+  fetch('/games')
+    .then(res => res.json())
+    .then(games => this.setState({ gameList: games, arraysReady: true }))
+    .catch(e => console.log(e));
+}
 
-    return (
-      <div>
-        <Container style={{ padding: "1%", margin: "2% 0" }}>
-          <h2>Most popular games:</h2>
-          <div
+render()  {
+  const { gameList, arraysReady } = this.state;
+
+  return (
+    <Grid columns={2}>
+      <Grid.Column width={3}>
+        <Card style={{ marginLeft: "3rem", height: "80vh" }}>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name='user'>22 Friends</Icon>
+          </Card.Content>
+        </Card>
+      </Grid.Column>
+      <Grid.Column width={10}>
+      <h2>Your favourite games:</h2>
+      <div
             style={{
               height: "3px",
               width: "100%",
@@ -34,12 +49,13 @@ export default class UserDashboard extends Component {
               marginBottom: "5%"
             }}
           />
-          {arraysReady ? (
-            <Card.Group itemsPerRow={3} stackable>
-              {userData.map(game => {
+        {arraysReady ? (
+          <Card.Group>
+            {gameList.map(game => {
                 return (
                   <Card
                     style={{
+                      width: "100%",
                       padding: "1%",
                       boxShadow: "4px 4px 2px 0px rgba(0,0,0,0.3)",
                       borderRadius: "20px"
@@ -49,7 +65,7 @@ export default class UserDashboard extends Component {
                     <Link to={`/game/${game.title}`} key={game.id}>
                       <Image
                         src={game.thumbnail}
-                        style={{ height: "10rem" }}
+                        style={{ height: "4rem" }}
                         centered
                       />
                       <Card.Content textAlign="center">
@@ -60,14 +76,14 @@ export default class UserDashboard extends Component {
                   </Card>
                 );
               })}
-            </Card.Group>
+          </Card.Group>
           ) : (
             <Segment loading>
               <Loader active inline="centered" />
             </Segment>
           )}
-        </Container>
-      </div>
-    );
+      </Grid.Column>
+    </Grid>
+);
+    }
   }
-}
