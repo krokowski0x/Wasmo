b9dec685c16f551360d97652a126248e0dd6259a
krokowski0x <krokowski.dx@gmail.com>
2019-05-29 20:11:44 +0200

    Added new endpoints

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -1,6 +1,6 @@
 const games = require('../db/mocks/games');
 const users = require('../db/mocks/users');
-const comments = require('../db/mocks/comments');
+let comments = require('../db/mocks/comments');
 const gameStats = require('../db/mocks/game_stats');
 
 const getAllGames = async (req, res) => {
@@ -12,6 +12,18 @@ const getGameById = async (req, res) => {
   const result = games.find(game => game.id === id);
 
   res.status(200).json(result);
+};
+
+const addNewGame = async (req, res) => {
+  const newGame = {
+    id: games.length,
+    title: req.body.title,
+    description: req.body.description,
+    thumbnail: req.body.thumbnail || "https://image.flaticon.com/icons/svg/141/141070.svg",
+  };
+
+  games.push(newGame);
+  res.status(200);
 };
 
 const getUserById = async (req, res) => {
@@ -28,6 +40,28 @@ const getCommentsByGameId = async (req, res) => {
   res.status(200).json(result);
 };
 
+const addCommentByGameId = async (req, res) => {
+  const gameId = Number(req.params.game_id);
+  const newComment = {
+    id: comments.length,
+    user_id: 1,
+    username: "krokowski0x",
+    game_id: gameId,
+    createdAt: new Date().toString(),
+    comment: req.body.comment,
+  };
+  
+  comments.push(newComment);
+  res.status(200);
+};
+
+const removeCommentByCommentId = async (req, res) => {
+  const commentId = Number(req.params.comment_id);
+  comments = comments.filter(comment => comment.id !== commentId);
+
+  res.status(200);
+};
+
 const getStatsByGameId = async (req, res) => {
   const gameId = Number(req.params.game_id);
   const result = gameStats.filter(statistic => statistic.game_id === gameId);
@@ -38,7 +72,10 @@ const getStatsByGameId = async (req, res) => {
 module.exports = {
   getAllGames,
   getGameById,
+  addNewGame,
   getUserById,
   getCommentsByGameId,
+  addCommentByGameId,
+  removeCommentByCommentId,
   getStatsByGameId
 };
--- server/server.js
+++ server/server.js
@@ -13,8 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/games', handlers.getAllGames);
 app.get('/games/:id', handlers.getGameById);
+app.post('/games', handlers.addNewGame);
 app.get('/users/:id', handlers.getUserById);
 app.get('/comments/:game_id', handlers.getCommentsByGameId);
+app.post('/comments/:game_id', handlers.addCommentByGameId);
+app.delete('/comments/:comment_id', handlers.removeCommentByCommentId);
 app.get('/game_stats/:game_id', handlers.getStatsByGameId);
 
 app.listen(API_PORT, () => {
